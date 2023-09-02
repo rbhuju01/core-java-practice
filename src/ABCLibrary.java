624cce2d67e03d19e8acadb49ed8786175c929c1
rashu bhuju <bhujurashu@gmail.com>
2023-09-01 18:35:24 -0600

    Added files in the develop

---
+++ src/ABCLibrary.java
@@ -0,0 +1,18 @@
+public class ABCLibrary {
+    //State
+    MembershipNumber;
+    Book_Author;
+    Book_Genres;
+    Book_Edition;
+    Book_Type;
+
+    //methods
+    checkMembership();
+    addBook();
+    reserveBook();
+    addToWaitList();
+    checkoutBook();
+
+
+
+}
---
+++ src/OnlineShopping.java
@@ -0,0 +1,23 @@
+public class OnlineShopping {
+
+        //State
+        Customer_Name;
+        Customer_Address;
+        Customer_Gender;
+        Customer_DOB;
+        Customer_CC;
+        Customer_Size;
+
+
+        //method
+        getCustomerDetails();
+        showCustomerPreferences();
+        getCustomerPaymentDetails();
+        payCustomerBalance();
+        addItems();
+        getShoeSize();
+        getTshirtSize();
+        getClothesCount();
+
+
+}
