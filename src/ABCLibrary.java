9a7631dfa8d4c0987a3669505ec04e8e911bf612
rashu bhuju <bhujurashu@gmail.com>
2023-09-01 18:04:00 -0600

    Added ABCLibrary,OnlineShopping,PatientPortal

---
+++ src/ABCLibrary.java
@@ -0,0 +1,20 @@
+public class ABCLibrary {
+    //State
+    MembershipNumber;
+    Book_Author;
+    Book_Genres;
+    Book_Edition;
+    Book_Type;
+
+    //methods
+    checkMembership();
+    addBook();
+    reserveBook();
+    addToWaitList();
+    checkoutBook();
+
+
+
+
+
+}
---
+++ src/PatientPortal.java
@@ -0,0 +1,21 @@
+public class PatientPortal {
+
+    Patient_Name;
+    Patient_DOB;
+    Patient_Address;
+    Patient_Insurance_Company;
+    Total_Bill;
+
+
+    //method
+    patientCheckIn();
+    addPatientDetails();
+    checkInsuranceDetail();
+    checkPatientBalance();
+    payPatientBalance();
+    updatePatientBalance();
+    updatePatientPaymentOption();
+
+
+
+}
