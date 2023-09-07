3486ac99e0791ff038aece5e072db2a3b233297f
rashu bhuju <bhujurashu@gmail.com>
2023-09-06 18:21:49 -0600

    added Claims.java

---
+++ src/Claims.java
@@ -0,0 +1,21 @@
+public class Claims {
+    /*
+    ClaimID
+    DriverID
+    Coverage
+    claimStatus
+    claimAdditionalDetails
+
+    getClaimID()
+    getDriverDetails()
+    getCoverageDetails()
+    createClaims()
+    submitClaim()
+    updateClaim()
+    getClaimStatus()
+    updateClaimStatus()
+    addClaimAdditionalDetails()
+    closeClaim()
+
+     */
+}
